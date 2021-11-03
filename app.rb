f4be84dcc78f78abdd8034aa64be0d6bd2574ba9
Myoung <mhbae93@gmail.com>
2021-11-04 00:14:13 +0900

    refactor to add form and render players

--- app.rb
+++ app.rb
@@ -1,12 +1,23 @@
 require 'sinatra/base'
-require "sinatra/reloader"
+require 'sinatra/reloader'
 
 class MyApp < Sinatra::Base
-    configure :development do
+  configure :development do
     register Sinatra::Reloader
-    end
-    
+  end
+
+  # get '/' do
+  #   'Testing infrastructure working!'
+  # end
+
   get '/' do
-    'Hello Battle!'
+    erb(:index)
+  end
+
+  post '/names' do
+    p params
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb(:play)
   end
 end
